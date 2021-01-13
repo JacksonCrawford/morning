0116731ae78029c1b43a8833a1040fd564bec21f
JacksonCrawford <57842293+JacksonCrawford@users.noreply.github.com>
2021-01-12 21:32:35 -0500

    Add files via upload

--- morning.sh
+++ morning.sh
@@ -5,18 +5,21 @@ using PyCall
 
 @pyimport pyowm
 
+cityName = "charlotte"
+countryName = "usa"
+
 owm = pyowm.OWM("d06e4a0c6d304d44be567dd5ae91ca43")
 wman = owm.weather_manager()
 
-function getWeather()
-        location = "Charlotte, USA"
+function getWeather(city, country)
+        location = city * ", " * country
         observation = wman.weather_at_place(location).weather
 
         return observation.temperature("fahrenheit")
 end
 
 function weather()
-        weather = getWeather()
+        weather = getWeather(cityName, countryName)
         
         text = "This morning's temperature is currently " * string(weather["temp"]) * " degrees, with a high of " * string(weather["temp_max"]) * " and a low of " * string(weather["temp_min"]) * " going into the rest of the day."
         return text
